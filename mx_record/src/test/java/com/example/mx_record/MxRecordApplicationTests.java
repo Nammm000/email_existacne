b298c09f0bec5064f65c6892aae5ed1ef3cfcee8
nam.nlh <nam.nlh@gmail.com>
2024-08-22 10:13:53 +0700

    mx_record

---
+++ mx_record/src/test/java/com/example/mx_record/MxRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mx_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
